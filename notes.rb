6b03a9df8dd37b34235f6dea34133297e5fe9631
jasonronalddavis <jdavis@c.ringling.edu>
2021-09-16 06:23:48 -0500

    added notes and seeded data

--- notes.rb
+++ notes.rb
@@ -1,1 +1,4 @@
 macbook = Ingredient.create(url: "https://www.amazon.com/Apple-MacBook-13-inch-256GB-Storage/dp/B08N5LLDSG/ref=sr_1_3?dchild=1&keywords=macbook+pro&qid=1631759803&sr=8-3", price: 1149.99, name: "macbook_pro")
+
+
+macbook = Ingredient.create(url: "https://www.amazon.com/Wacom-Cintiq-Drawing-Tablet-DTK1660K0A/dp/B07L77GTTY/ref=sr_1_3?dchild=1&keywords=cintiq&qid=1631775052&sr=8-3", price: 649.95, name: "Wacom Cyntiq Tablet")
